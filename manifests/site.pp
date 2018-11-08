28437f54ee3702ce070750b3118d6f1174be330d
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-08 21:17:05 -0200

    present again

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,11 @@
 node 'client.example.com' {
 
-  user { 'bruno.siva' :
+  user { 'bruno' :
     ensure     => present,
     uid        => '1000',
     gid        => '1000',
     shell      => '/bin/bash',
-    home       => '/home/bruno.silva',
+    home       => '/home/bruno',
   }
 
 
