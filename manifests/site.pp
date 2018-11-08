b60552c192a6e72855322d7a9c1eb3ec7bb7c287
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-08 19:31:01 -0200

    absent all

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node 'client.example.com' {
   user { 'bruno.siva' :
     home   => '/home/bruno.silva',
-    ensure => 'present',
+    ensure => 'absent',
   }
 
   file {'/home/bruno.silva/file-env-dev.txt' :
     require => User['bruno.siva'],
     content => 'Arquivo criado para o ambiente DEV',
-    ensure  => 'present',
+    ensure  => 'absent',
   }
 }
