6f9c41d0614fdbf8bcd029614f1836253479bf65
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-08 21:19:43 -0200

    Criand arquivo de teste no dev

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,8 @@
 node 'client.example.com' {
 
-  user { 'bruno' :
-    ensure     => present,
-    uid        => '1000',
-    gid        => '1000',
-    shell      => '/bin/bash',
-    home       => '/home/bruno',
-  }
-
-
-/*
-  file {'/home/bruno.silva/file-env-dev.txt' :
-    require => User['bruno.siva'],
+  file {'/home/vagrant/file-env-dev.txt' :
     content => 'Arquivo criado para o ambiente DEV',
     ensure  => 'present',
   }
-  */
+
 }
