fda0c62ef6095520b74cefed6e11f37074084033
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-09 21:42:15 -0200

    Adicionado no classificador, entradas para o node web.dev.example.com

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,27 +1,6 @@
-node 'client.example.com' {
-  file {'/home/vagrant/file-env-prod.txt' :
-    content => 'Conteúdo levado para o ambiente de PRODUCTION',
-    ensure  => 'present',
-  }
-}
-
 node 'web.dev.example.com' {
   file {'/home/vagrant/content.txt' :
     content => 'Conteúdo do ambiente de DEV.',
-    ensure  => 'present',
-  }
-}
-
-node 'web.hmg.example.com' {
-  file {'/home/vagrant/content.txt' :
-    content => 'Conteúdo do ambiente HMG.',
-    ensure  => 'present',
-  }
-}
-
-node 'web.prd.example.com' {
-  file {'/home/vagrant/content.txt' :
-    content => 'Conteúdo do ambiente PRD,',
     ensure  => 'present',
   }
 }
