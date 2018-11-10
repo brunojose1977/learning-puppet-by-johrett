98cbbdce08f81b0ec1c884c8032e40a4c0e4a06e
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-10 15:30:55 -0200

    Adicionado conteúdo em ambiente DEV.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,28 @@
 node 'client.example.com' {
-  file {'/home/vagrant/client.txt' :
-    content => 'Conteúdo de DEV.',
+  file {'/home/vagrant/publicacao.txt' :
+    content => 'Conteúdo de client.example.com',
+    ensure  => 'present',
+  }
+}
+
+node 'web1.example.com' {
+  file {'/home/vagrant/publicacao.txt' :
+    content => 'Conteúdo em ambiente DEV',
     ensure  => 'present',
   }
 }
 
 /*
-node 'web.dev.example.com' {
-  file {'/home/vagrant/content.txt' :
-    content => 'Conteúdo do ambiente de DEV.',
+node 'web2.example.com' {
+  file {'/home/vagrant/publicacao.txt' :
+    content => 'Conteúdo em ambiente APPROVAL',
+    ensure  => 'present',
+  }
+}
+
+node 'web3.example.com' {
+  file {'/home/vagrant/publicacao.txt' :
+    content => 'Conteúdo em ambiente PRODUCTION',
     ensure  => 'present',
   }
 }
