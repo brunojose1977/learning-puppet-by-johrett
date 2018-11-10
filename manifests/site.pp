ab177550194dfb84a9e33733e1a9262f66c9c066
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-09 22:02:18 -0200

    Conteúdo específico para client.example.com e ambiente dev.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,15 @@
+node 'client.example.com' {
+  file {'/home/vagrant/client.txt' :
+    content => 'Conteúdo de DEV.',
+    ensure  => 'present',
+  }
+}
+
+/*
 node 'web.dev.example.com' {
   file {'/home/vagrant/content.txt' :
     content => 'Conteúdo do ambiente de DEV.',
     ensure  => 'present',
   }
 }
+*/
