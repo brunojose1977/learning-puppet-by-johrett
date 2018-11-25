d4cb02342d3666456dbb5c5e1f129408400c1f95
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-25 09:47:00 -0200

    *

--- modules/mod_test3/manifests/init.pp
+++ modules/mod_test3/manifests/init.pp
@@ -1,6 +1,7 @@
 class mod_test3 {
-  file{ '/home/vagrant/mod_test3_file.txt' :
+  file{ '/home/vagrant/mod_test3/mod_test3_file.txt' :
     ensure  => 'present',
     content => 'Arquivo criado pelo módulo mod_test3.',
+    recurse => true,
   }
 }
