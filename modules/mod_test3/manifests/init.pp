02c472566ed2ccae0b838ed6206263bf6307d969
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-25 10:23:14 -0200

    *

--- modules/mod_test3/manifests/init.pp
+++ modules/mod_test3/manifests/init.pp
@@ -1,8 +1,7 @@
 class mod_test3 {
-  file{ '/home/vagrant/mod_test3/dir_child1/dir_child2' :
+  file{ '/home/vagrant/mod_test3/dir_child1/dir_child2/' :
     ensure  => 'directory',
     recurse => true,
-    recurselimit => 10,
     purge => true, # purge all unmanaged junk
     force => true, # also purge subdirs and links etc.
   }
