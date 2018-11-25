78c3554023bf8a1d7627b536d91b314687c43e22
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-25 11:47:10 -0200

    *

--- modules/mod_test3/manifests/init.pp
+++ modules/mod_test3/manifests/init.pp
@@ -1,19 +1,10 @@
 class mod_test3 {
 
-  notify { 'xxx Hello ${::fqdn}. xxx' : }
-
   notify { "aaa Hello ${::fqdn}. aaa" : }
-
-  notice ( "bbb Hello ${::fqdn}. bbb")    
-
-  notify { 'notify test1' :
-    message => 'yyy Hello ${::fqdn}. yyy',
-  }
 
   notify { 'notify test2' :
       message  => lookup('fqdn'),
   }
-
 
   file{ '/home/vagrant/mod_test3_file.txt' :
     ensure  => 'present',
