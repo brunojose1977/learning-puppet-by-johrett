20f08689c4d485186bd71f45926944163c8748a3
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-25 10:08:45 -0200

    *

--- modules/mod_test3/manifests/init.pp
+++ modules/mod_test3/manifests/init.pp
@@ -1,7 +1,6 @@
 class mod_test3 {
   file{ '/home/vagrant/mod_test3/dir_child1/dir_child2' :
     ensure  => 'directory',
-    source => '/home/vagrant/mod_test3/dir_child1/dir_child2',
     recurse => true,
     recurselimit => 3,
   }
