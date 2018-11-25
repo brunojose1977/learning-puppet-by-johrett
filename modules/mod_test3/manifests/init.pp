76491ce664d33292de5cc0b76d0c33840e3a8ed1
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-25 11:07:04 -0200

    *

--- modules/mod_test3/manifests/init.pp
+++ modules/mod_test3/manifests/init.pp
@@ -1,4 +1,9 @@
 class mod_test3 {
+
+  notify {
+    "### Hello ${::fqdn}, now mod_test3 will be applyed. ###":
+  }
+
   file{ '/home/vagrant/mod_test3_file.txt' :
     ensure  => 'present',
     content => 'Arquivo criado pelo mod_test3',
