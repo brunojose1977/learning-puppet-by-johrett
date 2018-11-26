c42d5e8ba7f97b361c7d2daea7d2903e361d174a
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-26 08:53:16 -0200

    *

---
+++ modules/mod_create_folders/manifests/init.pp
@@ -0,0 +1,13 @@
+class mod_create_folders {
+
+  exec { 'create folder projetos':
+    command => 'mkdir /home/vagrant/projetos',
+    path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    # refreshonly => true,
+  }
+
+  notify{ 'projetos folder notification' :
+    message => '\n\n Criada a pasta projetos\n\n'    
+  }
+
+}
