a4f7bd4a2b7145804a3a3b8d1859f1882be6dcec
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-25 23:12:39 -0200

    */

--- modules/mod_get_available_memory/manifests/init.pp
+++ modules/mod_get_available_memory/manifests/init.pp
@@ -2,7 +2,7 @@ class mod_get_available_memory {
 
   $available_system_memory = lookup('available_system_memory')
   $filename                = lookup('filename')
-  $url_module_file = "puppet:///modules/mod_get_available_memory/"+${filename}
+  $url_module_file = "puppet:///modules/mod_get_available_memory/${filename}"
 
   Notify { 'show availble system memory' :
     message => "\n\n
