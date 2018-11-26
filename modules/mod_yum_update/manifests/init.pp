65fc1739b3866c43009f5f2de1b4e984abaff75d
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-26 08:47:37 -0200

    *

--- modules/mod_yum_update/manifests/init.pp
+++ modules/mod_yum_update/manifests/init.pp
@@ -1,7 +1,7 @@
 class mod_yum_update {
 
   exec { 'Atualiza So by yum update':
-    command => '/usr/sbin/yum update -y',
+    command => 'yum update -y',
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     # refreshonly => true,
   }
