84199997d29bbcac99e496a87ba75c785dfb8bd9
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-25 22:26:51 -0200

    *

--- modules/mod_get_avalilable_memory/manifests/init.pp
+++ modules/mod_get_avalilable_memory/manifests/init.pp
@@ -3,10 +3,10 @@ class mod_get_avalilable_memory {
   $available_system_memory = lookup('available_system_memory')
 
   Notify { 'show availble system memory' :
-    message => '\n\n
-    Available System Memory: ${$available_system_memory}
-    \n\n'
-    
+    message => "\n\n
+    Available System Memory: ${available_system_memory}
+    \n\n"
+
   }
 
 }
