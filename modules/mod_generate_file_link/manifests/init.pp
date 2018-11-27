b6086b549eab6b0c2afbdb6c206f94020decd715
brunojose1977 <brunojose1977@yahoo.com.br>
2018-11-27 20:09:43 -0200

    *

--- modules/mod_generate_file_link/manifests/init.pp
+++ modules/mod_generate_file_link/manifests/init.pp
@@ -17,9 +17,14 @@ class mod_generate_file_link {
     \n\n Foram criados dois diretórios e depois foi feito um link simbólico entre eles:\n
       /home/vagrant/original_folder ----> /home/vagrant/mirrored_folder \n\n
 
-      A partir de agora, tudo que for escrito no original será replicado no espelhado
+      A partir de agora, tudo que for escrito no original será replicado no espelhado.
 
     \n\n"
+  }
+
+  file { 'Original folder sample file' :
+    ensure => 'file',
+    content => 'This is a original file placed in a original folder.',
   }
 
 }
